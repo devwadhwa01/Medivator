e5e3850326357ecd02ea8d4f46e10adcb19b5f84
Dev Wadhwa <devwadhwa01@gmail.com>
2021-06-05 01:03:19 +0530

    Final Stage 1

--- read_images.py
+++
@@ -1,9 +0,0 @@
-# import cv2
-#
-# l = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg']
-# for i in l:
-#     img = cv2.imread(f"finger_images/{i}")
-#     print(img.shape)
-#     img = cv2.resize(img,(200,200))
-#     print(img.shape)
-#     cv2.imwrite(f'resized/{i}', img)
